b8a4ccc21fc4611a32cc12f7814fb2f536c70056
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-03-17 17:36:37 +0530

    npm install tailwindcss @tailwindcss/vite vite.config.ts npm install @mui/material @emotion/react @emotion/styled import Button from '@mui/material/Button';

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+export const plugins = {
+  'postcss-import': {},
+  tailwindcss: {},
+  autoprefixer: {},
+};
+
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
