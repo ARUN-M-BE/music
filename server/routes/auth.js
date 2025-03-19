4c12c487e9848a064d57e7040ccf0d795021e796
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-03-19 15:44:00 +0530

    good complet 1

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,25 +1,46 @@
 const router = require("express").Router();
 
 const admin = require("../config/firebase.config");
+const user = require("../models/user");
 
-
-router.get("/login", async(req, res) => {
-    if (!req.headers.authorization) {
-        return res.status(500).json({message :"invalid token"});
+router.get("/login", async (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(500).json({ message: "invalid token" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+    const decodeValue = await admin.auth().verifyIdToken(token);
+    if (!decodeValue) {
+      return res.status(401).json({ message: "Unauthorized" });
+    } else {
+      const userExists = await user.findOne({ user_id: decodeValue.user_id });
+      if (!userExists) {
+        newUserData(decodeValue, req, res);
+      } else {
+        return res.send("Need to Update");
+      }
     }
-    const token = req.headers.authorization.split(" ")[1];
-    try {
-        const decodeValue = await admin.auth().verifyIdToken(token);
-        if (!decodeValue) {
-            return res.status(401).json({message :"Unauthorized"});
-        }
-        else {
-            return res.status(200).json(decodeValue);
-        }
-    } catch (error) {
-        res.status(505).json({message :"Error"});
-
-    }
+  } catch (error) {
+    res.status(505).json({ message: "Error" });
+  }
 });
+
+const newUserData = async (decodeValue, req, res) => {
+  const newUser = new user({
+    name : decodeValue.name,
+    email : decodeValue.email,
+    imageURL : decodeValue.picture,
+    user_id : decodeValue.user_id,
+    email_verified : decodeValue.email_verified,
+    role : "member",
+    auth_time: decodeValue.auth_time,
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(200).send({ user: savedUser });
+  } catch (error) {
+    res.status(400).send({ sucess: false, msg: error });
+  }
+};
 
 module.exports = router;
