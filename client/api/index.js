d5d8f5d144e9c8a9cd33da7f0b7658ba7452e483
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-04-09 23:53:28 +0530

    sxcsx

--- client/api/index.js
+++ client/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000/";
+const baseUrl = "https://g-music-pvze.onrender.com/";
 
 export const validateUser = async (token) => {
   try {
