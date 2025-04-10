0e19ed52692645cef108dfdf70dca33c2ca2ffbc
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-04-10 20:36:16 +0530

    api/v1/files

--- client/src/components/ArtistCard.jsx
+++ client/src/components/ArtistCard.jsx
@@ -60,7 +60,7 @@ const ArtistCard = ({ data, index }) => {
         return;
       }
   
-      const response = await fetch("https://g-music-pvze.onrender.com/delete", {
+      const response = await fetch("https://g-music-pvze.onrender.com/api/v1/files/delete", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
