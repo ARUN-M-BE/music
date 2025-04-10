ab47c138d3272928fc2972b1c1f3b81f66d03287
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-04-10 18:35:20 +0530

    aaa

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import './index.css';
 // import ErrorBoundary from "./ErrorBoundary";
 import { StateProvider } from "./context/stateProvider";
 import { initialState } from "./context/InitialState";
