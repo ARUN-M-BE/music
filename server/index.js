cad2b41a0e18a4b02b6943300a832d3d643a8127
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-04-11 07:36:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/media", mediaRoutes);
+//app.use("/api/media", mediaRoutes);
 
 app.use("/api/media", require("./routes/media"));
 
