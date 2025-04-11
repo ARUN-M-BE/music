ab1af074e373762377e6f5de73d4e2b1faabd0fa
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-04-11 07:59:38 +0530

    Update media.js

--- server/routes/media.js
+++ server/routes/media.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const multer = require("multer");
+const dotenv = require("dotenv");
+const imagekit =require("imagekit");
 //import multer from "multer";
-const imagekit = require ("../imagekit.js";);
+//const imagekit = require ("../imagekit.js";);
 
-const router = express.Router();
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+//const router = express.Router();
+//const storage = multer.memoryStorage();
+//const upload = multer({ storage });
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const imagekit = new ImageKit({
+  urlEndpoint: process.env.VITE_IMAGEKIT_URL_ENDPOINT,
+  publicKey: process.env.VITE_IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.VITE_IMAGEKIT_PRIVATE_KEY,
+});
+const PORT = process.env.IMAGEKIT_PORT || 3001;
 
 /**
  * POST /api/media/upload
  * Upload an image and return fileId and other metadata
- */
+ 
 router.post("/upload", upload.single("file"), async (req, res) => {
   try {
     const file = req.file;
@@ -33,7 +48,7 @@ router.post("/upload", upload.single("file"), async (req, res) => {
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
   }
-});
+});*/
 
 /**
  * DELETE /api/media/delete/:fileId
