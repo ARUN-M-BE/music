ff51dfae27145a9c660c78da656267891658ae3e
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-04-11 07:42:27 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 // Fallback for SPA
 //app.get('*', (req, res) => {
  // res.sendFile(path.join(__dirname, 'client/dist', //'index.html'));
-});
+//});
 
 
 
