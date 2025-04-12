b45fddc5beed62e5b43de5aefa489de4b6d57c30
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-04-12 15:02:46 +0530

    gud bad VITE_API_URL

--- client/api/route.js
+++ client/api/route.js
@@ -1,7 +1,7 @@
 import ImageKit from "imagekit";
 
 
-const baseURL = import.meta.env.API_URL || "http://localhost:3000/" || "http://localhost:3001/";
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000/" || "http://localhost:3001/";
 
 const imagekit = new ImageKit({
         urlEndpoint: import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT,
