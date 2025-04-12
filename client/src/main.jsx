7c8800cd2bcce0f785ff2cde401bcb135f9e703d
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-04-12 15:45:18 +0530

    main jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-// import ErrorBoundary from "./ErrorBoundary";
+import ErrorBoundary from "./ErrorBoundary";
 import { StateProvider } from "./context/stateProvider";
 import { initialState } from "./context/InitialState";
 import reducer from "./context/reducer";
@@ -12,13 +11,13 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    {/* <ErrorBoundary> */}
+  // <React.StrictMode>
+     <ErrorBoundary> 
     <Router>
       <StateProvider initialState={initialState} reducer={reducer}>
         <App />
       </StateProvider>
     </Router>
-    {/* </ErrorBoundary> */}
-  </React.StrictMode>
+    </ErrorBoundary>
+  // </React.StrictMode>
 );
