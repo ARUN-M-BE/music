b3f44208857a10c165f56486544de7d45b68e9ff
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-04-12 14:58:24 +0530

    import.meta.env

--- client/api/index.js
+++ client/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.API_URL || "http://localhost:3000/" || "http://localhost:3001/";
+const baseURL = import.meta.env.API_URL || "http://localhost:3000/" || "http://localhost:3001/";
 
 export const validateUser = async (token) => {
   try {
