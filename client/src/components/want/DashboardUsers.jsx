953a5e40c72fdec587bf72122df8cba9daed2a70
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-04-12 15:40:03 +0530

    user coe

--- client/src/components/want/DashboardUsers.jsx
+++ client/src/components/want/DashboardUsers.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useStateValue } from '../context/stateProvider';
+import { useStateValue } from '../../context/stateProvider';
 import { motion } from 'framer-motion';
 
 const DashboardUsers = () => {
